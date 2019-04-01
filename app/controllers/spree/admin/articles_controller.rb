448b43fa524bc5c45dfdb460af3d8c43aaa48726
Antonino <antoninoscaffidi@gmail.com>
2019-04-01 18:38:04 +0200

    Added article image

--- app/controllers/spree/admin/articles_controller.rb
+++ app/controllers/spree/admin/articles_controller.rb
@@ -1,14 +1,20 @@
 module Spree
   module Admin
     class ArticlesController < ResourceController
-      
+    
       def index
         @articles = Spree::Article.all
       end
-        
+
+       
       private
+      
+      def find_resource
+        Article.friendly.find(params[:id])
+      end
+
       def article_params
-        params.require(:article).permit(:title, :body)
+        params.require(:article).permit(:title, :body, :article_img)
       end
 
     end
--- app/models/spree/article.rb
+++ app/models/spree/article.rb
@@ -1,12 +1,16 @@
 module Spree
   class Article < Spree::Base
+   
     extend FriendlyId
     validates :title, presence: true
-    
+   
     friendly_id :title, use: %i[slugged history]
-
+    
+    has_one_attached :article_img
+     
+    private 
     def should_generate_new_friendly_id?
-      slug.blank? || name_changed?
+      slug.blank? || title_changed?
     end
 
   end
